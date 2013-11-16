c25b162301b89133b31b82bb147db9a7d2d88e86
Geoff <me@geoffreychong.com>
2013-11-16 21:27:22 +1100

    Fixed style.css link

--- application/development/library/extensions/styles.php
+++ application/development/library/extensions/styles.php
@@ -29,7 +29,7 @@ function lt3_load_styles()
    */
   wp_register_style('lt3_custom_admin_styles', LT3_FULL_STYLES_PATH
     . '/admin/custom-admin-styles.css', array(), LT3_SCRIPTS_CACHE_BREAK);
-  wp_register_style('lt3_main_stylesheet', get_template_directory()
+  wp_register_style('lt3_main_stylesheet', get_template_directory_uri()
     . '/style.css', array(), LT3_STYLES_CACHE_BREAK);
 
   wp_enqueue_style('lt3_main_stylesheet');
